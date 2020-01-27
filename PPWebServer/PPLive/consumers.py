cc89dc53d6d590fe3d31e1ac8e95dcd63a465217
alecwhardy <alecwhardy@gmail.com>
2020-01-26 16:42:41 -0800

    Changes to UI

--- PPWebServer/PPLive/consumers.py
+++ PPWebServer/PPLive/consumers.py
@@ -1,11 +1,20 @@
 from channels.generic.websocket import WebsocketConsumer
 
+import re
+
 class CtrlConsumer(WebsocketConsumer):
+    
+    jog_regex = re.compile('J[UD]S\d')
+
     def connect(self):
         self.accept()
 
     def disconnect(self, close_code):
+        # What even does close_code do?!?
         pass
 
     def receive(self, text_data):
-        pass
+        if self.jog_regex.match(text_data) is not None:
+            print(text_data)
+        else:
+            print("Weird command received: " + text_data)
--- PPWebServer/PPLive/static/PPLive/control.js
+++ PPWebServer/PPLive/static/PPLive/control.js
@@ -1,3 +1,5 @@
+var SOCKET_WRITE_DELAY = 100;
+
 var ctrlSocket = new WebSocket('ws://' + window.location.host + '/ws/ctrl/');
 
 ctrlSocket.onmessage = function(data) {
@@ -6,7 +8,7 @@ ctrlSocket.onmessage = function(data) {
 };
 
 ctrlSocket.onclose = function(data) {
-    console.error('Chat socket closed unexpectedly');
+    console.error('Socket closed unexpectedly');
 };
 
 /*
@@ -14,3 +16,41 @@ ctrlSocket.send(JSON.stringify({
     'message': message
 }));
 */
+
+function getTravelSpeed(){
+    return $("#jogSpeedRange").val();
+}
+
+// Winch Control Buttons
+var timeoutId = 0;
+function handleWinchCtrlButton(event){
+    
+    var speed = String($("#jogSpeedRange").val());
+    var cmdString = "";
+    switch(event.target.id){
+        case "winchUp":
+            // Jog Up Speed *    
+            cmdString = "JUS" + speed;
+            break;
+        case "winchDown":
+            // Jog Down Speed *
+            cmdString = "JDS" + speed;
+            break;
+        default:
+            alert("Functionality not yet supported");
+            break;
+    }
+    console.log(cmdString);
+    ctrlSocket.send(cmdString);
+
+    
+    timeoutId = setTimeout(handleWinchCtrlButton, 100, event);
+}
+$(document).ready(function() {
+    // Handle winch ctrl buttons
+    $('.winchCtrl').on('mousedown', function(event) {
+        handleWinchCtrlButton(event);
+    }).on('mouseup mouseleave', function() {
+        clearTimeout(timeoutId);
+    });
+});
